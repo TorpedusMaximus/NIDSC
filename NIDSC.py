26aa0167bca5f2e8847d2e481f8a25b47667bde5
Kacper Małkowski <malkowski.kacper@gmail.com>
2021-06-12 21:52:21 +0200

    wiekszosc metod

--- NIDSC.py
+++ NIDSC.py
@@ -1,16 +1,74 @@
 from functools import reduce
+from random import randint
+
 import numpy as np
 import operator as op
 
 
-bits=np.random.randint(0,2,16)
+def generateMemory(DRAM):
+    for i in range(0, len(DRAM[0])):
+        DRAM[i] = np.random.randint(0, 2, len(DRAM[0]))
+        DRAM[i][reduce(op.xor, [i for i, bit in enumerate(DRAM[i]) if bit])] = not DRAM[i][
+            reduce(op.xor, [i for i, bit in enumerate(DRAM[i]) if bit])]
+    return DRAM.transpose()
 
-print(bits)
 
-print(reduce(op.xor,[i for i, bit in enumerate(bits) if bit]))
+def checkHammingCodes(hammingBlock):
+    hammingBlock[reduce(op.xor, [i for i, bit in enumerate(hammingBlock) if bit])] = not hammingBlock[
+        reduce(op.xor, [i for i, bit in enumerate(hammingBlock) if bit])]
+    return hammingBlock
 
-bits[reduce(op.xor,[i for i, bit in enumerate(bits) if bit])]=not bits[reduce(op.xor,[i for i, bit in enumerate(bits) if bit])]
 
-print(bits)
+def rowHammer(DRAM, row):
+    print(row)
+    print(DRAM)
+    for i in range(0, len(DRAM[row])):
+        if randint(1, 2) == 1:
+            DRAM[row][i] = not DRAM[row][i]
+    print(DRAM)
+    print("\n")
 
-print(reduce(op.xor,[i for i, bit in enumerate(bits)]))
+
+def rowAccess(DRAM, row):
+    rowData = DRAM[row]
+    error = randint(1, 100)
+    if error <= 2:
+        if error == 2:
+            if row >= len(DRAM[0]) - 1:
+                rowHammer(DRAM, row - 1)
+            else:
+                rowHammer(DRAM, row + 1)
+        else:
+            if row <= 0:
+                rowHammer(DRAM, row + 1)
+            else:
+                rowHammer(DRAM, row + 1)  # zmiana
+
+    return rowData
+
+
+def memoryAccess(DRAM, test):
+    for i in range(0, (len(DRAM[0]))):
+        test[i] = rowAccess(DRAM, i)
+
+
+def testMemory(DRAM):
+    test = np.zeros((len(DRAM[0]), len(DRAM[0])))
+    memoryAccess(DRAM, test)
+    for i in range(0, (len(DRAM[0]))):
+        hammingBlock = test[:, i]
+        test[:, i] = checkHammingCodes(hammingBlock)
+    return test
+
+
+def main():
+    howMuchBits = 16
+    DRAM = np.zeros((howMuchBits, howMuchBits))
+    DRAM = generateMemory(DRAM)
+
+    DRAM = testMemory(DRAM)
+    print(DRAM)
+
+
+if __name__ == "__main__":
+    main()
